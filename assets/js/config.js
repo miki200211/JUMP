38665559d346aa847d2a888812363a2a6010e65b
miki200211 <miki200211@users.noreply.github.com>
2026-07-10 14:04:30 +0800

    Update API_BASE to successfully tested Web App URL

--- assets/js/config.js
+++ assets/js/config.js
@@ -4,7 +4,7 @@
  */
 const CONFIG = {
   // Replace this placeholder with your deployed Google Apps Script Web App URL
-  API_BASE: 'https://script.google.com/macros/s/AKfycbzP2orS7OikY3_-g7wu2zb8SIP9Yv7Qbs3qhLRiU8PGMc2biFFq4k4K64HWWKU4SIJS/exec',
+  API_BASE: 'https://script.google.com/macros/s/AKfycbxJUyoiXda7ux-JaC_jB6eXKd55OtEQCFFEYlD_J1EPnS4VPzYl7xSm00oLZlNuyH20/exec',
   
   // Polling interval in milliseconds. Must be at least 5000ms to preserve execution quota.
   POLL_INTERVAL_MS: 5000,
